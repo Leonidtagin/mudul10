dffc349028425775dc7023bd605ad3b5f2c7edc8
Игорёха <leonidtagin401@gmail.com>
2024-12-17 01:31:48 +1100

    ...

---
+++ Многопроцессное считывание.py
@@ -0,0 +1,28 @@
+import multiprocessing
+from datetime import datetime
+
+def read_info(name):
+    all_date = []
+    with open(name, 'r') as file1:
+        while True:
+            line = file1.readline().strip()
+            all_date.append(line)
+            if not line:
+                break
+
+files = ['file1.txt', 'file2.txt', 'file3.txt', 'file4.txt']
+start1 = datetime.now()
+for f in files:
+    print(f)
+    read_info(f)
+
+end1 = datetime.now()
+time_of_line_function = end1 - start1
+print(f'Время работы линейного вызова: {time_of_line_function}')
+if __name__ == '__name__':
+    start2 = datetime.now()
+    with multiprocessing.Pool(processes=4) as pool:
+        pool.map(read_info, files)
+    end2 = datetime.now()
+    time_of_line_multiprocessing = end2 - start2
+    print(f'Время работы мультипроцесса: {time_of_line_multiprocessing}')
